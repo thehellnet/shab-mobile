f7084479c59dfc7bdefcca1ecbf99b471c084294
sardylan <sardylan@gmail.com>
2016-07-17 15:05:02 +0200

    Adding map activity

--- app/src/main/java/org/thehellnet/shab/mobile/activity/MainActivity.java
+++ app/src/main/java/org/thehellnet/shab/mobile/activity/MainActivity.java
@@ -1,67 +1,14 @@
 package org.thehellnet.shab.mobile.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.thehellnet.shab.mobile.R;
-import org.thehellnet.shab.mobile.SHAB;
-import org.thehellnet.shab.mobile.service.ShabService;
 
-public class MainActivity extends AppCompatActivity {
-
-    private MenuItem menuEnabled;
-    private MenuItem menuConfig;
+public class MainActivity extends ShabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        menuEnabled = menu.findItem(R.id.menu_enabled);
-        menuConfig = menu.findItem(R.id.menu_config);
-        updateMenu();
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_enabled:
-                toggleShabService();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    private void updateMenu() {
-        menuEnabled.setChecked(SHAB.isServiceRunning(ShabService.class));
-        menuEnabled.setIcon(SHAB.isServiceRunning(ShabService.class)
-                ? R.drawable.menu_enabled_on
-                : R.drawable.menu_enabled_off);
-
-        menuConfig.setEnabled(!SHAB.isServiceRunning(ShabService.class));
-    }
-
-    private void toggleShabService() {
-        Intent intent = new Intent(SHAB.getAppContext(), ShabService.class);
-        if (!SHAB.isServiceRunning(ShabService.class)) {
-            startService(intent);
-        } else {
-            stopService(intent);
-        }
-        updateMenu();
     }
 }
---
+++ app/src/main/java/org/thehellnet/shab/mobile/activity/MapActivity.java
@@ -0,0 +1,36 @@
+package org.thehellnet.shab.mobile.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.thehellnet.shab.mobile.R;
+
+public class MapActivity extends ShabActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMapObject) {
+        googleMap = googleMapObject;
+
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
