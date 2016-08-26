935b7092c2eaf6557e328f69e51df01390bd9bf0
sardylan <sardylan@gmail.com>
2016-08-27 01:36:18 +0200

    RawLog autoscroll and autodelete old lines

--- app/src/main/java/org/thehellnet/shab/mobile/activity/fragment/RawLogFragment.java
+++ app/src/main/java/org/thehellnet/shab/mobile/activity/fragment/RawLogFragment.java
@@ -4,6 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.joda.time.DateTime;
@@ -21,15 +25,21 @@ public class RawLogFragment extends ShabFragment {
         @Override
         public void onReceive(Context context, Intent intent) {
             Line line = (Line) intent.getSerializableExtra("line");
-            if (rawlogText != null && line != null) {
-                rawlogText.append(String.format("%s - %s\n", DateTime.now(), line.serialize()));
+            if (rawlogText != null
+                    && scrollView != null
+                    && line != null) {
+                appendLogLine(line);
             }
         }
     }
 
+    private static final String TAG = RawLogFragment.class.getSimpleName();
+    private static final int RAWLOG_MAX_LINES = 150;
+
     private NewLineReceiver newLineReceiver;
 
     private TextView rawlogText;
+    private ScrollView scrollView;
 
     @Override
     protected int getLayout() {
@@ -46,6 +56,7 @@ public class RawLogFragment extends ShabFragment {
         super.initElements();
 
         rawlogText = (TextView) getActivity().findViewById(R.id.rawlog_text);
+        scrollView = (ScrollView) getActivity().findViewById(R.id.rawlog_scroll_container);
     }
 
     @Override
@@ -64,5 +75,23 @@ public class RawLogFragment extends ShabFragment {
         }
 
         super.onPause();
+    }
+
+    private void appendLogLine(Line line) {
+        rawlogText.append(String.format("%s - %s\n", DateTime.now(), line.serialize()));
+
+        int linesToRemove = rawlogText.getLineCount() - RAWLOG_MAX_LINES;
+        if (linesToRemove > 0) {
+            for (int i = 0; i < linesToRemove; i++) {
+                Editable text = rawlogText.getEditableText();
+                int lineStart = rawlogText.getLayout().getLineStart(0);
+                int lineEnd = rawlogText.getLayout().getLineEnd(0);
+                text.delete(lineStart, lineEnd);
+            }
+        }
+
+        Log.d(TAG, String.valueOf(rawlogText.getLineCount()));
+
+        scrollView.fullScroll(View.FOCUS_DOWN);
     }
 }
